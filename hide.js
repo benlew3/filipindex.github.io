7db860a938129b98ae460846bdf69c721def1f28
benjamin lew <bennru33@gmail.com>
2016-12-06 12:55:16 -0800

    converting master to gh-pages

---
+++ hide.js
@@ -0,0 +1,161 @@
+$(document).ready(function(){
+    $(".profile1").click(function(){
+    	$("#Al").toggle();
+      $(".profile1").toggle();
+    });
+    $("#Al").click(function(){
+      $(".profile1").toggle();
+      $("#Al").toggle();
+    });
+
+    $(".profile2").click(function(){
+    	$("#Ben").toggle();
+      $(".profile2").toggle();
+    });
+    $("#Ben").click(function(){
+      $(".profile2").toggle();
+      $("#Ben").toggle();
+    });
+
+    $(".profile3").click(function(){
+      $("#Flo").toggle();
+      $(".profile3").toggle();
+    });
+    $("#Flo").click(function(){
+      $(".profile3").toggle();
+      $("#Flo").toggle();
+    });
+
+    $(".profile4").click(function(){
+    	$("#Jez").toggle();
+      $(".profile4").toggle();
+    });
+    $("#Jez").click(function(){
+      $(".profile4").toggle();
+      $("#Jez").toggle();
+    });
+
+    $(".profile5").click(function(){
+      $("#Josh").toggle();
+      $(".profile5").toggle();
+    });
+    $("#Josh").click(function(){
+      $(".profile5").toggle();
+      $("#Josh").toggle();
+    });
+
+    $(".profile6").click(function(){
+    	$("#Kyla").toggle();
+      $(".profile6").toggle();
+    });
+    $("#Kyla").click(function(){
+      $(".profile6").toggle();
+      $("#Kyla").toggle();
+    });
+
+    $(".profile7").click(function(){
+    	$("#Lolo").toggle();
+      $(".profile7").toggle();
+    });
+    $("#Lolo").click(function(){
+      $(".profile7").toggle();
+      $("#Lolo").toggle();
+    });
+
+    $(".profile8").click(function(){
+    	$("#MJ").toggle();
+      $(".profile8").toggle();
+    });
+    $("#MJ").click(function(){
+      $(".profile8").toggle();
+      $("#MJ").toggle();
+    });
+
+    $(".profile9").click(function(){
+    	$("#Myles").toggle();
+      $(".profile9").toggle();
+    });
+    $("#Myles").click(function(){
+      $(".profile9").toggle();
+      $("#Myles").toggle();
+    });
+
+    $(".profile10").click(function(){
+    	$("#Rizza").toggle();
+      $(".profile10").toggle();
+    });
+    $("#Rizza").click(function(){
+      $(".profile10").toggle();
+      $("#Rizza").toggle();
+    });
+
+    $(".post").click(function(){
+      $(".docs").toggle();
+      $(".docs2").hide();
+      $(".docs3").hide();
+      $(".docs4").hide();
+      $(".docs5").hide();
+      $(".docs6").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post2").click(function(){
+      $(".docs2").toggle();
+      $(".docs").hide();
+      $(".docs3").hide();
+      $(".docs4").hide();
+      $(".docs5").hide();
+      $(".docs6").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post3").click(function(){
+      $(".docs3").toggle();
+      $(".docs").hide();
+      $(".docs2").hide();
+      $(".docs4").hide();
+      $(".docs5").hide();
+      $(".docs6").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post4").click(function(){
+      $(".docs4").toggle();
+      $(".docs").hide();
+      $(".docs3").hide();
+      $(".docs2").hide();
+      $(".docs5").hide();
+      $(".docs6").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post5").click(function(){
+      $(".docs5").toggle();
+      $(".docs").hide();
+      $(".docs3").hide();
+      $(".docs4").hide();
+      $(".docs2").hide();
+      $(".docs6").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post6").click(function(){
+      $(".docs6").toggle();
+      $(".docs").hide();
+      $(".docs3").hide();
+      $(".docs4").hide();
+      $(".docs5").hide();
+      $(".docs2").hide();
+      $(".docs7").hide();
+    });
+
+    $(".post7").click(function(){
+      $(".docs7").toggle();
+      $(".docs").hide();
+      $(".docs3").hide();
+      $(".docs4").hide();
+      $(".docs5").hide();
+      $(".docs6").hide();
+      $(".docs2").hide();
+    });
+});
